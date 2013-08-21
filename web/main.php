642924f6fd061f449bdd7bdc5cae43c45de9ef3e
analytalica <analytalica@gmail.com>
2013-08-21 17:57:06 -0500

    Added version sync line

--- web/main.php
+++ web/main.php
@@ -14,5 +14,6 @@ A player-minute is defined as a single minute spent connected to the server by a
 The pureLog Web Interface allows users to view the daily player-minute totals from the web in a graphical, visualized format.
 Results are compiled at the beginning of each new day, so the results of the current day will only be available the day after.</p>
 <p><a href="http://purebattlefield.org">Version 1.0.1 - by Analytalica</a></p>
+<p>Currently synced with pureLog Server Edition 1.2</p>
 
 <?php include("config.php"); ?>
